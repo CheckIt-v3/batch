f44a30942c01278b652d72f5b844e2a8c04b8480
jiminsong490 <misosjm@naver.com>
2023-07-21 17:30:56 +0900

    feat : redis로 중복확인 (url을 key로 설정)

--- src/main/java/com/techeer/checkitbatch/CheckitbatchApplication.java
+++ src/main/java/com/techeer/checkitbatch/CheckitbatchApplication.java
@@ -1,18 +1,19 @@
 package com.techeer.checkitbatch;
 
+import com.techeer.checkitbatch.domain.selenium.Selenium;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 
-@EnableBatchProcessing
+//@EnableBatchProcessing
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class CheckitbatchApplication {
 
 	public static void main(String[] args) {
-//		SpringApplication.run(CheckitbatchApplication.class, args).getBean(Selenium.class).crawling();
-		SpringApplication.run(CheckitbatchApplication.class, args);
+		SpringApplication.run(CheckitbatchApplication.class, args).getBean(Selenium.class).crawling();
+//		SpringApplication.run(CheckitbatchApplication.class, args);
 	}
 
 }
