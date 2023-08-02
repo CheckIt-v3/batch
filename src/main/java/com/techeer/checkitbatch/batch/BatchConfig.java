53214d22e1a4e77345bf4d51ee7a5169b83aa491
jiminsong490 <misosjm@naver.com>
2023-08-03 00:13:15 +0900

    feat : newBook Collection 삭제 및 스캐줄링 적용

--- src/main/java/com/techeer/checkitbatch/batch/BatchConfig.java
+++ src/main/java/com/techeer/checkitbatch/batch/BatchConfig.java
@@ -2,6 +2,7 @@ package com.techeer.checkitbatch.batch;
 
 import com.techeer.checkitbatch.batch.step.Step1Config;
 //import com.techeer.checkitbatch.batch.step.Step3Config;
+import com.techeer.checkitbatch.batch.step.Step5Config;
 import com.techeer.checkitbatch.batch.step.Step6Config;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,16 +12,17 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.HashMap;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Slf4j
 @Configuration
+@EnableScheduling
 @RequiredArgsConstructor
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 public class BatchConfig {
     private final JobBuilderFactory jobBuilderFactory;
     private final Step1Config step1Config;
+    private final Step5Config step5Config;
     private final Step6Config step6Config;
 
 //    private final Step3Config step3Config;
@@ -29,10 +31,11 @@ public class BatchConfig {
     public Job job(){
         return jobBuilderFactory.get("Crawling Data Insert DB")
             .start(step1Config.crawling()) // 크롤링
-            //.next() 몽고디비에 크롤링 데이터 저장
 //            .next(step3Config.mongodbToMySQLStep())
+                .next(step5Config.dropNewBook())
                 .next(step6Config.setRedisKey()) // redis에 key 저장
                 .build();
     }
+
 
 }
